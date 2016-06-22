474eb465c885ef36b90d94cf8a56d10ab5ad9ac8
Thomas Maurin <thomas@maurin.io>
2016-06-23 00:04:23 +0200

    chore: wording

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,6 @@
 def fibs(n):
     """
-    Yield numbers in the Fibonacci sequence up to its n-th number
+    Yield numbers in the Fibonacci sequence up to its n-th item
     """
     a, b, i = 0, 1, 0
     while i <= n:
@@ -10,6 +10,7 @@ def fibs(n):
 n = int(input("Give me an int: "))
 gen = fibs(n)
 
+# Deplete the generator while retaining its last-yielded value in result
 result = None
 for result in gen:
     pass
